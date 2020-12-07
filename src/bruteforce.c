2400beccae830cb0a090cadb091eed0fdd1a9f9c
krissemicolon <krissemicolon@gmail.com>
2020-12-07 21:04:30 +0100

    testing gpg

---
+++ src/bruteforce.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdlib.h>
+
+char lowercase_alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+char uppercase_alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char numbers[] = "0123456789";
+char special_chars[] = "!@#$%^&*.,;:-_(){}[]";
+
+
+int main() {
+    printf(lowercase_alphabet);
+
+    return 0;
+}
+
+bruteforce(int lenght) {
+    
+
+    return lowercase_alphabet;
+}
