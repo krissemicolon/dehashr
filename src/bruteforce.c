29b11af68f6777ca5bbc1eeeca4902882284f724
Daniel Florescu <Daniel.florescu1905@protonmail.ch>
2021-01-26 12:44:02 +0100

    Fixed program not compiling.

--- src/bruteforce.c
+++ src/bruteforce.c
@@ -146,14 +146,3 @@ void generate(int maxlen) {
     free(letters);
     free(buffer);
 }
-
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-		fprintf(stderr, "Usage: %s Length\n", argv[0]);
-		exit(1);
-    }
-
-    generate(atoi(argv[1]));
-    return 0;
-}
-
